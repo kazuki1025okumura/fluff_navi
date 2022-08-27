259da6de7e6905070865f219131bc7a657146c94
kazukiokumura <kazuki1025okumura@gmail.com>
2022-08-27 22:50:33 +0900

    Add: sorceryをインストールして必要なファイルを生成 #7

--- Gemfile
+++ Gemfile
@@ -66,3 +66,4 @@ gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 gem 'net-smtp'
 gem 'net-imap'
 gem 'net-pop'
+gem 'sorcery', '0.16.3'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
+
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
+
+  resources :users, only: %i[new create]
 end
