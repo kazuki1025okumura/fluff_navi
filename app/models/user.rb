91f59314b761dd3d3628ae196c3982529fe2f624
kazukiokumura <kazuki1025okumura@gmail.com>
2022-08-28 15:47:46 +0900

    Fix: user.rbのタイプミスを修正 #11

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
-  validates :email, uniquieness: true, presence: true
+  validates :email, uniqueness: true, presence: true
   validates :name, presence: true, length: { maximum: 10 }
 end
