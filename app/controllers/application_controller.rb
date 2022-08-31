d0b267a4276386198d6d7fce3bcd3459334e0ae8
kazukiokumura <kazuki1025okumura@gmail.com>
2022-08-31 22:07:57 +0900

    Add: ログイン画面を作成 #16

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  add_flash_types :info, :success, :warning, :error
+  before_action :require_login
+
+  private
+
+  def not_authenticated
+    redirect_to login_path, Error: 'Please login first.'
+  end
 end
--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,2 +1,20 @@
 class UserSessionsController < ApplicationController
+  skip_before_action :require_login, only: %i[new create]
+
+  def new; end
+
+  def create
+    @user = login(params[:email], params[:password])
+    if @user
+      redirect_back_or_to root_path, success: t('.success')
+    else
+      flash.now[:error] = t('.error')
+      render :new
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to root_path, success: t('.success')
+  end
 end
