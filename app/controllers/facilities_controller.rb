985adca9d4bb56039bd20b02451a62d97b58f629
kazukiokumura <kazuki1025okumura@gmail.com>
2022-09-01 14:39:04 +0900

    Add: 施設一覧画面を作成してトップページにリンクを作成 #17

---
+++ app/controllers/facilities_controller.rb
@@ -0,0 +1,8 @@
+class FacilitiesController < ApplicationController
+  skip_before_action :require_login, only: %i[index show]
+  def index
+    @facility = Facility.all.order(created_at: :desc)
+  end
+
+  def show; end
+end
