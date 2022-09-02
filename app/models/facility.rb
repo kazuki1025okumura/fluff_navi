13a5f1a82379940cf24eae06e5226b9ac1ff8a60
kazukiokumura <kazuki1025okumura@gmail.com>
2022-09-03 00:06:41 +0900

    Add: active_hashを設定して県を登録 #24

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -1,4 +1,7 @@
 class Facility < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+
   has_many :facility_categories, dependent: :destroy
   has_many :categories, through: :facility_categories
   has_many :managements, dependent: :destroy
---
+++ app/models/prefecture.rb
@@ -0,0 +1,5 @@
+class Prefecture < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' }, { id: 2, name: '東京都' }, { id: 3, name: '神奈川県' }
+  ]
+end
