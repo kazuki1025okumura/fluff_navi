223a09b6f090af07e2d484ea927e7236716b407d
kazukiokumura <kazuki1025okumura@gmail.com>
2022-09-02 09:48:50 +0900

    Add: Animalモデルを作成 #22

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_28_062641) do
+ActiveRecord::Schema.define(version: 2022_09_02_004244) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "animals", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_animals_on_name", unique: true
+  end
 
   create_table "facilities", force: :cascade do |t|
     t.string "name", null: false
