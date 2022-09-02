183424ebfaafc0e0b214128a626b7c1c5212a427
kazukiokumura <kazuki1025okumura@gmail.com>
2022-09-02 15:40:17 +0900

    Fix: 施設と動物のアソシエーションを追加 #39

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -1,6 +1,8 @@
 class Facility < ApplicationRecord
   has_many :facility_categories, dependent: :destroy
   has_many :categories, through: :facility_categories
+  has_many :managements, dependent: :destroy
+  has_many :animals, through: :managements
 
   validates :name, presence: true
 end
