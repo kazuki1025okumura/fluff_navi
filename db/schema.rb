645345fa211ab0f5dacf586ef3079705fc21c048
kazukiokumura <kazuki1025okumura@gmail.com>
2022-09-03 17:34:34 +0900

    Add: animalsテーブルに漢字カラムを追加 #24

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_02_144628) do
+ActiveRecord::Schema.define(version: 2022_09_03_083147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2022_09_02_144628) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "kanji"
     t.index ["name"], name: "index_animals_on_name", unique: true
   end
 
