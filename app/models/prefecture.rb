ac85366aae14ce2dc4fad5640a28e463c2f5e28c
kazukiokumura <kazuki1025okumura@gmail.com>
2022-09-04 20:38:17 +0900

    Add: レイアウトなどを調整 #24

--- app/models/prefecture.rb
+++ app/models/prefecture.rb
@@ -1,5 +1,5 @@
 class Prefecture < ActiveHash::Base
   self.data = [
-    { id: 1, name: '---' }, { id: 2, name: '東京都' }, { id: 3, name: '神奈川県' }
+    { id: 1, name: '東京都' }, { id: 2, name: '神奈川県' }
   ]
 end
