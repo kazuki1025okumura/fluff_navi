3b3f383778bfd62b7d84a2c4d95b4455af66d7b3
kazukiokumura <kazuki1025okumura@gmail.com>
2022-09-09 12:09:17 +0900

    Add: CarrierWaveをインストールして初期設定 #51

---
+++ app/uploaders/avatar_uploader.rb
@@ -0,0 +1,16 @@
+class AvatarUploader < CarrierWave::Uploader::Base
+
+  storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def default_url
+    'default_avatar.png'
+  end
+
+  def extension_allowlist
+    %w[jpg jpeg gif png]
+  end
+end
