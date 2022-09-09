30e21415207e544827bb01e50e2bdf150f4142e1
kazukiokumura <kazuki1025okumura@gmail.com>
2022-09-09 16:35:07 +0900

    Add: 画像のプレビュー表示の関数を設定 #51

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "stylesheets/application.css"
+import "custom/preview"
 require('jquery')
 
 Rails.start()
