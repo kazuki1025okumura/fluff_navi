af26d57b8a3f1d7f9107a8de98186f4bcac7a82c
kazukiokumura <kazuki1025okumura@gmail.com>
2022-09-30 14:35:47 +0900

    Add:ユーザー情報をもとにオススメの施設をランダムで取得するメソッドを作成 #76

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,15 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  scope :favo_animal, ->(animal) { Facility.joins(:managements).where('animal_id = ?', animal.id) }
+  scope :favo_category, ->(category) { Facility.joins(:facility_categories).where('category_id = ?', category.id) }
+
+  def suggest_to_login_user(user)
+    animal = user.animal
+    category = user.category
+    facilities = Facility.offset(rand(Facility.count)).limit(100)
+    suggest_animal_facilities = facilities.favo_animal(animal).to_a
+    suggest_category_facilities = facilities.favo_category(category).to_a
+    suggest_animal_facilities.concat(suggest_category_facilities).uniq.sample(3)
+  end
 end
