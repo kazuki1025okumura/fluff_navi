0500b7b7bd0a05ac33fc21b046d3fffce30cfdf5
kazukiokumura <kazuki1025okumura@gmail.com>
2022-10-03 13:57:52 +0900

    Add: チェックボックスを設置し、ログイン状態を半年間保持できるように設定 #91

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,7 +4,7 @@ class UserSessionsController < ApplicationController
   def new; end
 
   def create
-    @user = login(params[:email], params[:password])
+    @user = login(params[:email], params[:password], params[:remember])
     if @user
       redirect_back_or_to root_path, success: t('.success')
     else
--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -4,7 +4,7 @@
 # Available submodules are: :user_activation, :http_basic_auth, :remember_me,
 # :reset_password, :session_timeout, :brute_force_protection, :activity_logging,
 # :magic_login, :external
-Rails.application.config.sorcery.submodules = [:reset_password]
+Rails.application.config.sorcery.submodules = [:reset_password, :remember_me]
 
 # Here you can configure each submodule's features.
 Rails.application.config.sorcery.configure do |config|
@@ -318,8 +318,8 @@ Rails.application.config.sorcery.configure do |config|
 
     # How long in seconds the session length will be
     # Default: `60 * 60 * 24 * 7`
-    #
-    # user.remember_me_for =
+    # 自動ログインの期間を半年に設定
+    user.remember_me_for = 15552000
 
     # When true, sorcery will persist a single remember me token for all
     # logins/logouts (to support remembering on multiple browsers simultaneously).
