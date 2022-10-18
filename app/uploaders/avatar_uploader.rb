c92b963b8ee50702ef0bf26f91aef170644cc118
kazukiokumura <kazuki1025okumura@gmail.com>
2022-10-18 20:02:56 +0900

    Add: 本番環境の画像を表示する設定 #109

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,6 +1,11 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
-  storage :file
+
+  if Rails.env.development? || Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
