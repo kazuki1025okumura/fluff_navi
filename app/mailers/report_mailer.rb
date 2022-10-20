9c4c9c276f959a4705383c58c0c1723f5fe8408a
kazukiokumura <kazuki1025okumura@gmail.com>
2022-10-20 12:13:57 +0900

    Fix: mailerの設定を変更 #113

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -2,7 +2,7 @@ class ReportMailer < ApplicationMailer
   def send_mail
     @report = params[:report]
     mail(
-      to: 'report@example.com',
+      to: ENV['ADMIN_EMAIL_ADDRESS'],
       subject: '報告通知'
     )
   end
