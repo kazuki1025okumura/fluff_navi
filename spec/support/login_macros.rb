2144205c15e685a21e912e48e56a31ce84877c68
kazukiokumura <kazuki1025okumura@gmail.com>
2022-10-28 20:23:28 +0900

    Add: サポートモジュールを使用してログイン機能を再現 #136

--- spec/support/login_macros.rb
+++ spec/support/login_macros.rb
@@ -1,9 +1,8 @@
 module LoginMacros
   def login_as(user)
-    visit root_path
-    click_link 'Login'
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: 'password'
-    click_button 'Login'
+    visit login_path
+    fill_in 'email', with: user.email
+    fill_in 'password', with: 'password'
+    click_button 'ログイン'
   end
 end
