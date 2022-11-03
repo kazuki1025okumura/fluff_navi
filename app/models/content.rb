3c737aa0cee9340dbc93927cdd97fbf58ed78363
kazukiokumura <kazuki1025okumura@gmail.com>
2022-11-04 02:00:42 +0900

    Fix: 報告のセレクトボックスを変更 #145

--- app/models/content.rb
+++ app/models/content.rb
@@ -2,7 +2,6 @@ class Content < ActiveHash::Base
   self.data = [
     { id: 1, name: '施設の情報変更' },
     { id: 2, name: '動物の飼育環境など' },
-    { id: 3, name: '動物への虐待、暴言など' },
-    { id: 4, name: 'その他' }
+    { id: 3, name: 'その他' }
   ]
 end
