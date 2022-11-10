25ec9d5d563d233f5f30c278b49e12501c702c75
kazukiokumura <kazuki1025okumura@gmail.com>
2022-11-10 15:24:19 +0900

    Fix: N+1問題を修正 #163

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   skip_before_action :require_login, only: %i[index]
 
   def index
-    @posts = Post.all.sample(12)
-    @facilities = Facility.where(suggest: 1).includes(%i[facility_categories categories managements animals]).sample(3)
+    @posts = Post.all.includes(%i[facility]).sample(12)
+    @facilities = Facility.where(suggest: 1).includes(%i[facility_categories categories managements animals posts]).sample(3)
   end
 end
