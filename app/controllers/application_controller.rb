1569be0bdaeb111a28dec134460def9ca0909147
kazukiokumura <kazuki1025okumura@gmail.com>
2022-11-28 22:33:41 +0900

    Add: 定数を設定 #85

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
+  ZOOM_LEVEL_SHOW_MAP = 15
+  ZOOM_LEVEL_INDEX_MAP = 8.7
+  LAT_INDEX_MAP = 35.509889
+  LNG_INDEX_MAP = 139.409462
+  ZOOM_LEVEL_TOKYO = 9.3
+  ZOOM_LEVEL_KANAGAWA = 9.3
+  LAT_TOKYO = 35.699627
+  LNG_TOKYO = 139.468790
+  LAT_KANAGAWA = 35.405050
+  LNG_KANAGAWA = 139.367149
+
+
   add_flash_types :info, :success, :warning, :error
   before_action :require_login
 
