aeb79295f4a0d3c056b6f6886011242476fda1f7
kazukiokumura <kazuki1025okumura@gmail.com>
2022-12-01 22:33:14 +0900

    Add:seed_fuのファイルに施設追加用のコードを設定 #172

--- db/fixtures/03_facility.rb
+++ db/fixtures/03_facility.rb
@@ -34,4 +34,21 @@ CSV.read('lib/csv/kanagawa.csv', headers: true).each do |row|
     s.id = row['id']
     s.suggest = row['suggest']
   end
+
+  CSV.read('lib/csv/add_facility.csv', headers: true).each do |row|
+    Facility.seed do |s|
+      s.name = row['name']
+      s.description = row['description']
+      s.animal_ids = row['animal_ids']&.split(',')
+      s.category_ids = row['category_ids']&.split(',')
+      s.address = row['address']
+      s.longitude = row['longitude']
+      s.latitude = row['latitude']
+      s.tel_number = row['tel_number']
+      s.opening_hours = row['opening_hours']
+      s.web_site = row['web_site']
+      s.prefecture_id = row['prefecture_id']
+      s.id = row['id']
+      s.suggest = row['suggest']
+    end
 end
